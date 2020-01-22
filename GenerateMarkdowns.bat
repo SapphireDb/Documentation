fa2312c0a48f45a1f9d5fd0a80a8c5ae41e151cb
morrisjdev <morrisj@live.de>
2020-01-22 12:39:50 +0100

    added readme generation code

--- GenerateMarkdowns.bat
+++ GenerateMarkdowns.bat
@@ -1,1 +1,6 @@
+markdown-pp ./sapphiredb-js.README.mdpp -o ../sapphiredb-js/README.md
+markdown-pp ./sapphiredb.README.mdpp -o ../sapphiredb-js/projects/sapphiredb/README.md
+markdown-pp ./ng-sapphiredb.README.mdpp -o ../sapphiredb-js/projects/ng-sapphiredb/README.md
 
+markdown-pp ./Server/sapphiredb.README.mdpp -o ../SapphireDb/README.md
+markdown-pp ./Server/sapphiredb-nohtml.README.mdpp -o ../SapphireDb/README-NUGET.md
